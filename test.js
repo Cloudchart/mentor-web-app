24c5cd917dbe8f6001508d4ae32a3b4508a9c05b
Eugene Kovalev <seanchas@gmail.com>
2015-10-29 13:45:27 +0300

    Removed test.js from root

--- test.js
+++
@@ -1,5 +0,0 @@
-import ThemeStorage from './storage/NewThemeStorage'
-
-ThemeStorage.loadAvailableThemesForUser('62906a29-719e-46f6-8bdd-1836a3811e12')
-  .then(console.log)
-  .catch(console.error)
