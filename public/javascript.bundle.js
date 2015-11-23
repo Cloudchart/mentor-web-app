29485598dfba1cac1d34a03688dd71a6b550cfef
Andrey Peresleguine <peresleguine@gmail.com>
2015-11-23 16:55:23 +0300

    javascript.bundle.js

--- public/javascript.bundle.js
+++ public/javascript.bundle.js
@@ -54259,7 +54259,7 @@
 
 /***/ },
 /* 480 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
@@ -54270,7 +54270,7 @@
 	function checkRemotePermission(permissionData) {
 	  if (permissionData.permission == 'default') {
 	    // This is a new web service URL and its validity is unknown.
-	    window.safari.pushNotification.requestPermission(SAFARI_PUSH_URL, SAFARI_PUSH_ID, {}, checkRemotePermission);
+	    window.safari.pushNotification.requestPermission(("https://mentorpush.herokuapp.com/safari"), ("web.com.mentor.notifications"), {}, checkRemotePermission);
 	  } else if (permissionData.permission == 'denied') {
 	    // The user said no. Talk to your UX expert to see what you can do to entice your
 	    // users to subscribe to push notifications.
@@ -54292,8 +54292,8 @@
 
 	function requestSafariPush() {
 	  // Ensure that the user can receive Safari Push Notifications.
-	  if (SAFARI_PUSH_ID && SAFARI_PUSH_URL && 'safari' in window && 'pushNotification' in window.safari) {
-	    var permissionData = window.safari.pushNotification.permission(SAFARI_PUSH_ID);
+	  if (("web.com.mentor.notifications") && ("https://mentorpush.herokuapp.com/safari") && 'safari' in window && 'pushNotification' in window.safari) {
+	    var permissionData = window.safari.pushNotification.permission(("web.com.mentor.notifications"));
 	    checkRemotePermission(permissionData);
 	  } else {
 	    // A good time to let a user know they are missing out on a feature or just bail out completely?
