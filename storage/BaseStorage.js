7920ef54c192529155c7a799fb76c8b559cbdeb4
Eugene Kovalev <seanchas@gmail.com>
2016-04-25 11:40:12 +0300

    Initialize pub/sub with first request on load/clear

--- storage/BaseStorage.js
+++ storage/BaseStorage.js
@@ -1,9 +1,17 @@
+import Redis from 'ioredis'
 import DataLoader from 'dataloader'
 import models from '../models'
 
 import {
   mapReduce
 } from './utils'
+
+
+let createBroadcastPayload = (id) =>
+  JSON.stringify({
+    pid:  process.pid,
+    id:   id
+  })
 
 
 let basicFinder = (storageName, modelName, ids) =>
@@ -22,13 +30,72 @@ let createStorage = (storageName, options = {}) => {
 
   let loader = new DataLoader(finder, { cache: options.cache !== false })
 
-  let loaderMethods = ['load', 'loadMany', 'clear', 'clearAll']
-    .reduce(
-      (memo, name) => {
-        memo[name] = loader[name].bind(loader)
-        return memo
-      }, {}
-    )
+  let pubRedis = null
+  let subRedis = null
+
+  let _initializeSub = () => {
+    if (subRedis) return
+
+    subRedis = new Redis()
+
+    subRedis.subscribe(`mentor:${modelName}:clear`, `mentor:${modelName}:clearAll`)
+
+    subRedis.on('message', (channel, message) => {
+      let [_, name, action] = channel.split(':')
+
+      if (name !== modelName) return
+
+      let payload = null
+      try { payload = JSON.parse(message) } catch(error) {}
+
+      if (!payload || payload.pid === process.pid) return
+
+      if (action === 'clear') {
+        console.log('Receibed clear request for id:', payload.id, storageName)
+        return loaderMethods.clear(payload.id)
+      }
+
+      if (action === 'clearAll') {
+        console.log('Receibed clear request for all records', storageName)
+        return loaderMethods.clearAll()
+      }
+    })
+  }
+
+  let _initializePub = () => {
+    if (pubRedis) return
+
+    pubRedis = new Redis()
+  }
+
+  let _initializePubSub = () => {
+    _initializePub()
+    _initializeSub()
+  }
+
+  let loaderMethods = {
+    load: (id) => {
+      _initializePubSub()
+      return loader.load(id)
+    },
+    loadMany: (ids) => {
+      _initializePubSub()
+      return loader.loadMany(ids)
+    },
+    clear: (id, shouldBroadcast) => {
+      _initializePubSub()
+      if (shouldBroadcast)
+        pubRedis.publish(`mentor:${modelName}:clear`, createBroadcastPayload(id))
+      return loader.clear(id)
+    },
+
+    clearAll: (shouldBroadcast) => {
+      _initializePubSub()
+      if (shouldBroadcast)
+        pubRedis.publish(`mentor:${modelName}:clearAll`, createBroadcastPayload())
+      return loader.clearAll()
+    }
+  }
 
   let queriesMethods      = {}
 
@@ -96,24 +163,25 @@ let createStorage = (storageName, options = {}) => {
 
     create: (attributes = {}) =>
       create(attributes)
-        .then(record => loader.clear(record.id).load(record.id))
+        .then(record => loaderMethods.clear(record.id, true).load(record.id))
     ,
 
     createMany: (attributesArray = []) =>
       createMany(attributesArray)
+        .then(result => loaderMethods.clearAll(true))
         .then(result => null)
 
     ,
 
     update: (id, attributes = {}) =>
       update(id, attributes)
-        .then(record => loader.clear(id).load(id))
+        .then(record => loaderMethods.clear(id, true).load(id))
     ,
 
     destroy: async (id) => {
       let record = await loader.load(id)
       await destroy(record.id)
-        .then(() => loader.clear(id))
+        .then(() => loaderMethods.clear(id, true))
         .then(() => null)
       await options.afterDestroy && options.afterDestroy(record)
       return null
