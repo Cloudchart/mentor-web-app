e5943efad947c6911848cc1fe50cd09afbd897bc
Eugene Kovalev <seanchas@gmail.com>
2016-05-25 12:23:33 +0300

    Default values for InsightOrigin

--- graphql/types/InsightOriginType.js
+++ graphql/types/InsightOriginType.js
@@ -13,18 +13,22 @@ export default new GraphQLObjectType({
 
     author: {
       type: new GraphQLNonNull(GraphQLString),
+      resolve: ({ author }) => author || ''
     },
 
     url: {
       type: new GraphQLNonNull(GraphQLString),
+      resolve: ({ url }) => url || ''
     },
 
     title: {
       type: new GraphQLNonNull(GraphQLString),
+      resolve: ({ title }) => title || ''
     },
 
     duration: {
       type: new GraphQLNonNull(GraphQLInt),
+      resolve: ({ duration }) => duration || 0
     },
 
   })
